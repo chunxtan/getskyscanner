a5cfb626a3e4f9d2f625908b106ce2602de732e3
chunxtan <99042026+chunxtan@users.noreply.github.com>
2023-11-24 22:36:32 +0800

    add accomms api

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const KEY = process.env.EXPRESS_SKYSCANNER_API_KEY;
+const SKYSCANNER_KEY = process.env.EXPRESS_SKYSCANNER_API_KEY;
+const HOTEL_KEY = process.env.EXPRESS_HOTELS4_API_KEY;
 
 app.post('/api/getCodes', async (req,res) => {
 
@@ -18,7 +19,7 @@ app.post('/api/getCodes', async (req,res) => {
   const fetchOptions = {
     method: 'POST',
     headers: {
-      'x-api-key': `${KEY}`,
+      'x-api-key': `${SKYSCANNER_KEY}`,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(req.body)
@@ -40,7 +41,7 @@ app.post('/api/getFlights', async (req, res) => {
   const fetchOptions = {
     method: 'POST',
     headers: {
-      'x-api-key': `${KEY}`,
+      'x-api-key': `${SKYSCANNER_KEY}`,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(req.body)
@@ -54,6 +55,55 @@ app.post('/api/getFlights', async (req, res) => {
     console.error(err);
   }
 })
+
+app.get('/api/getAccommsDestId', async (req, res) => {
+  const baseUrl = 'https://hotels4.p.rapidapi.com/locations/v3/search';
+
+  const queryParams = req.query;
+
+  const url = new URL(baseUrl);
+  url.search = new URLSearchParams(queryParams).toString();
+
+  const fetchOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': `${HOTEL_KEY}`,
+      'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
+    }
+  }
+
+  try {
+    const response = await fetch(url, fetchOptions);
+    const returnRes = await response.json();
+    res.json(returnRes);
+  } catch(err) {
+    console.error(err);
+  }
+})
+
+app.post('/api/getAccomms', async (req, res) => {
+
+  const url = 'https://hotels4.p.rapidapi.com/properties/v2/list';
+
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': `${HOTEL_KEY}`,
+      'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
+    },
+    body: JSON.stringify(req.body)
+  }
+
+  try {
+    const response = await fetch(url, fetchOptions);
+    const returnRes = await response.json();
+    res.json(returnRes);
+  } catch(err) {
+    console.error(err);
+  }
+})
+
 
 const port = process.env.PORT || 8080
 
